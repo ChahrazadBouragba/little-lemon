f93055a7eb48846794e7a30c077f5afe71930225
ChahrazadBouragba <chbouragba25@gmail.com>
2023-08-26 00:09:08 +0100

    Heading elements added

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,6 +1,22 @@
+
+import logo from '../imges/Logo.png';
+
 export default function Heading() {
     return (
         <>
+        <header>
+            <img src={logo} alt="logo"/>
+            <nav>
+                <ul>
+                    <li><a href="/Hero"/>Home</li>
+                    <li><a href="/About"/>About</li>
+                    <li><a href="/Highlights"/>Menu</li>
+                    <li><a href="/Hero"/>Reservations</li>
+                    <li><a href="/Highlights"/>Order Online</li>
+                    <li><a href="/Home"/>Login</li>
+                </ul>
+            </nav>
+        </header>
         </>
     )
 }
