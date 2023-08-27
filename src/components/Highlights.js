86f012fa4c0a67eb742bba2a92deb9c5b58f0047
ChahrazadBouragba <chbouragba25@gmail.com>
2023-08-27 16:18:01 +0100

    udpated-highlights

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,6 +1,7 @@
 import fish from '../imges/fish.png'
 import brushetta from '../imges/bruchetta.png'
 import lemonDessert from '../imges/lemon dessert.jpg'
+import delever from '../imges/DELEVER.png';
 
 export default function Highlights() {
     return (
@@ -15,14 +16,14 @@ export default function Highlights() {
             <article>
                 <img src={fish} className="fishImg"/>
                 <div className="card-description">
-                    <div>
+                    <div className="pricing-div">
                         <h2>Greek Salad</h2>
                         <p>$12.99</p>
                     </div>
                     <p>The famous greek salad of crispy lettuce, peppers,olives and our Chicago style teta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-                    <div>
+                    <div className="card-delivery-div">
                         <h3>Order a delivery</h3>
-                        <img/>
+                        <img src={delever} className="delever"/>
                     </div>
                 </div>
             </article>
@@ -30,14 +31,14 @@ export default function Highlights() {
             <article>
                 <img src={brushetta} className="brushettaImg"/>
                 <div className="card-description">
-                    <div>
+                    <div className="pricing-div">
                         <h2>Bruchetta</h2>
                         <p>$5.99</p>
                     </div>
                     <p>Our Bruschetta is made from grilled bread that has been smeared with garlic, olive, fresh farm tomato, cheese and seasoned with salt and olive oil.</p>
-                    <div>
+                    <div className="card-delivery-div">
                         <h3>Order a delivery</h3>
-                        <img/>
+                        <img src={delever} className="delever"/>
                     </div>
                 </div>
             </article>
@@ -45,15 +46,15 @@ export default function Highlights() {
             <article>
                 <img src={lemonDessert} className="dessertImg"/>
                 <div className="card-description">
-                <div>
+                <div className="pricing-div">
                     <h2>Lemon Dessert</h2>
                     <p>$12.99</p>
                 </div>
                 <p>This sweet lemon dessert comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
-                <div>
-                    <h3>Order a delivery</h3>
-                    <img/>
-                </div>
+                <div className="card-delivery-div">
+                        <h3>Order a delivery</h3>
+                        <img src={delever} className="delever"/>
+                    </div>
                 </div>
             </article>
             </div>
